547607f6b6027521f56b98294fb1a7266fafbaaa
Nathan Smithies <nathensmithies@gmail.com>
2022-07-08 20:29:09 +1200

    Added option to require rotational symmetry for interchanges

--- interchange.py
+++ interchange.py
@@ -1,12 +1,14 @@
 import argparse
 import json
 
+import numpy as np
+
 import belt_balancer
 from direction import Direction
 import optimisations
 from solver import Grid
 from template import EdgeMode
-from util import invert_components, set_literal, set_number, set_numbers
+from util import invert_components, set_literal, set_number, set_numbers, set_numbers_equal
 
 
 def prevent_passing(grid: Grid):
@@ -42,10 +44,10 @@ def prevent_awkward_underground_entry(grid: Grid):
         inv_direction = direction.reverse
         across_direction = direction.next
         for block in grid.iterate_tile_blocks(across_direction.vec, 3, direction.vec, 3, EdgeMode.NO_WRAP):
-            block[0, 1:3] = None  # Unimportant tiles
-
             if (block == None).any():
                 continue
+
+            block[0, 1:3] = None  # Unimportant tiles
 
             grid.clauses.append(invert_components([
                 *invert_components(block[0, 0].all_direction),
@@ -67,12 +69,22 @@ def prevent_awkward_underground_entry(grid: Grid):
             ]))
 
 
+def require_rotational_symmetry(grid: Grid):
+    for tile_a, tile_b in zip(grid.tiles.flatten(), np.rot90(grid.tiles, 2).flatten()):
+        grid.clauses += set_numbers_equal(
+            tile_a.input_direction + tile_a.output_direction + tile_a.underground + [tile_a.is_splitter],
+            tile_b.output_direction + tile_b.input_direction + tile_b.underground + [tile_b.is_splitter],
+        )
+        grid.clauses.append([-tile_a.is_splitter_head, -tile_b.is_splitter_head])
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Finds an interchange for building composite balancers')
     parser.add_argument('width', type=int, help='Interchange width')
     parser.add_argument('height', type=int, help='Combined balancer size')
     parser.add_argument('--underground-length', type=int, default=4, help='Sets the maximum length of underground section (excludes ends)')
     parser.add_argument('--alternating', action='store_true', help='Restrict output colours to an alternating pattern')
+    parser.add_argument('--rot-symmetry', action='store_true', help='Restrict output to rotationally symmetric interchanges')
     parser.add_argument('--all', action='store_true', help='Generate all belt balancers')
     parser.add_argument('--solver', type=str, default='Glucose3', help='Backend SAT solver to use')
     parser.add_argument('--partial', type=argparse.FileType('r'), help='Partial interchange to base solution from')
@@ -118,11 +130,17 @@ if __name__ == '__main__':
     grid.prevent_intersection(EdgeMode.NO_WRAP)
     grid.enforce_maximum_underground_length(EdgeMode.NO_WRAP)
 
-    optimisations.expand_underground(grid)
-    optimisations.apply_generic_optimisations(grid)
-    optimisations.break_vertical_symmetry(grid)
-    optimisations.break_horisontal_symmetry(grid)
-    optimisations.prevent_spirals(grid)
+    if args.rot_symmetry:
+        require_rotational_symmetry(grid)
+        optimisations.expand_underground(grid)
+        optimisations.prevent_small_loops(grid)
+        optimisations.prevent_empty_along_underground(grid, EdgeMode.NO_WRAP)
+        optimisations.prevent_belt_hooks(grid, EdgeMode.NO_WRAP)
+        optimisations.prevent_mergeable_underground(grid, EdgeMode.NO_WRAP)
+        optimisations.prevent_semicircles(grid, EdgeMode.NO_WRAP)
+        optimisations.prevent_underground_hook(grid, EdgeMode.NO_WRAP)
+    else:
+        optimisations.apply_generic_optimisations(grid)
 
     prevent_passing(grid)
 
