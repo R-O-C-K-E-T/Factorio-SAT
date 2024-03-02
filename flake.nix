21439fdc130d539f77f280e4f83b18f98c488a8a
Jacob Birkett <jacob@birkett.dev>
2024-03-02 02:28:45 -0700

    flake: inherit packages from applied overlays

--- flake.nix
+++ flake.nix
@@ -10,16 +10,18 @@
       inherit (nixpkgs) lib;
 
       eachSystem = lib.genAttrs (import systems);
-      pkgsFor =
-        eachSystem (system: import nixpkgs { localSystem.system = system; });
+      pkgsFor = eachSystem (system:
+        import nixpkgs {
+          localSystem.system = system;
+          overlays = [ self.overlays.default ];
+        });
     in {
       overlays.default = import ./nix/overlay.nix;
 
-      packages = eachSystem (system:
-        let pkgs = pkgsFor.${system};
-        in pkgs.callPackage ./nix/packages.nix { } // {
-          default = self.packages.${system}.factorio-sat;
-        });
+      packages = eachSystem (system: {
+        inherit (pkgsFor.${system}) factorio-sat factorio-sat-cli;
+        default = self.packages.${system}.factorio-sat;
+      });
 
       formatter = eachSystem (system: nixfmt.packages.${system}.default);
     };
